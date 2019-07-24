b3bf6fbaba2e3035e39dedc3722e72600bc748d0
Dustin Hamano <dhamano@Dustins-iMac-Pro.local>
2019-07-24 16:16:28 -0700

    replaced HOC with hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeFeature, buyItem } from './store/actions/';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
@@ -13,33 +11,20 @@ class App extends React.Component {
     this.state = {}
   }
 
-  removeFeature = item => {
-    this.props.removeFeature(item);
-  };
-
-  buyItem = item => {
-    this.props.buyItem(item);
-  };
-
   render () {
     return (
       <div className="boxes">
         <div className="box">
-          <Header car={this.props.car} />
-          <AddedFeatures car={this.props.car} removeFeature={this.removeFeature} />
+          <Header />
+          <AddedFeatures />
         </div>
         <div className="box">
-          <AdditionalFeatures store={this.props.store} buyItem={this.buyItem} />
-          <Total car={this.props.car} additionalPrice={this.props.additionalPrice} />
+          <AdditionalFeatures />
+          <Total />
         </div>
       </div>
     );
   }
 };
 
-const mapStateToProps = state => ({
-  additionalPrice: state.additionalPrice,
-  car: state.car,
-  store: state.store
-});
-export default connect( mapStateToProps, { removeFeature, buyItem })(App);
+export default App
--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { REMOVE_FEATURE } from '../store/reducers';
 
 const AddedFeature = props => {
+  const dispatch = useDispatch();
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick={props.removeFeature}>X</button>
+      <button className="button" onClick={() => dispatch({ type: REMOVE_FEATURE, payload: props.feature })}>X</button>
       {props.feature.name}
     </li>
   );
